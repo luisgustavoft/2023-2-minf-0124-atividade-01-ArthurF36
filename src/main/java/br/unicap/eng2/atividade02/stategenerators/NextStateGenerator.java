215cbae8aa8236c18d6ccb3523f720d2a8fdf1db
ArthurF36 <101573046+ArthurF36@users.noreply.github.com>
2023-08-31 10:52:59 -0300

    Update stategenerators

--- src/main/java/br/unicap/eng2/atividade02/stategenerators/NextStateGenerator.java
+++ src/main/java/br/unicap/eng2/atividade02/stategenerators/NextStateGenerator.java
@@ -2,10 +2,17 @@ package br.unicap.eng2.atividade02.stategenerators;
 
 import br.unicap.eng2.atividade02.states.State;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NextStateGenerator {
     private State stateCurrent;
 
     public NextStateGenerator(State stateCurrent) {
         this.stateCurrent = stateCurrent;
+    }
+
+    public Iterable<State> generate() {
+        List<State> next = new ArrayList<>();
     }
 }
